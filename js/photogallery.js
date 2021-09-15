2f20e89b43ace911eec6594c66a8161986bc47c7
Chantsova Anastasia <stasyadiv@gmail.com>
2021-09-15 19:11:59 +0300

    Add newFlows

---
+++ js/photogallery.js
@@ -0,0 +1,25 @@
+//Photogallery
+const gallerySet = document.querySelector(".gallery__set");
+const photoModal = document.querySelector(".photoModal");
+const bigPhotoImg = document.querySelector(".photoModal__img");
+
+gallerySet.addEventListener('click', openBigImg);
+
+function openBigImg(event) {
+    const imageLink = event.target.getAttribute("src");
+    bigPhotoImg.setAttribute("src", imageLink);
+    const imageAlt = event.target.getAttribute("alt");
+    bigPhotoImg.setAttribute("alt", imageAlt);
+    
+    gallerySet.removeEventListener('click', openBigImg);
+    photoModal.classList.add("is-open");
+    refs.bodyEl.classList.add("not-scroll");
+    photoModal.addEventListener('click', closeBigImg);
+}
+
+function closeBigImg(event) {
+    photoModal.removeEventListener('click', closeBigImg);
+    photoModal.classList.remove("is-open");
+    refs.bodyEl.classList.remove("not-scroll");
+    gallerySet.addEventListener('click', openBigImg);
+}
