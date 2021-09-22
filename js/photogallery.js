c780c05dcc30a0ec03d0d3a82b2e22c7a762b0ae
Chantsova Anastasia <stasyadiv@gmail.com>
2021-09-22 20:08:38 +0300

    Bugfix js

--- js/photogallery.js
+++ js/photogallery.js
@@ -6,15 +6,20 @@ const bigPhotoImg = document.querySelector(".photoModal__img");
 gallerySet.addEventListener('click', openBigImg);
 
 function openBigImg(event) {
-    const imageLink = event.target.getAttribute("src");
-    bigPhotoImg.setAttribute("src", imageLink);
-    const imageAlt = event.target.getAttribute("alt");
-    bigPhotoImg.setAttribute("alt", imageAlt);
-    
-    gallerySet.removeEventListener('click', openBigImg);
-    photoModal.classList.add("is-open");
-    refs.bodyEl.classList.add("not-scroll");
-    photoModal.addEventListener('click', closeBigImg);
+    if (event.target.classList.contains('gallery__img')) {
+        const imageLink = event.target.getAttribute("src");
+        bigPhotoImg.setAttribute("src", imageLink);
+        const imageAlt = event.target.getAttribute("alt");
+        bigPhotoImg.setAttribute("alt", imageAlt);
+        
+        gallerySet.removeEventListener('click', openBigImg);
+        photoModal.classList.add("is-open");
+        refs.bodyEl.classList.add("not-scroll");
+        photoModal.addEventListener('click', closeBigImg);
+    }
+    else {
+        return;
+    }
 }
 
 function closeBigImg(event) {
